934742d1f8bd409c835deee125671c7a08a2ae40
Gabriel <gabriellf.dev@gmail.com>
2023-02-22 15:46:42 -0300

    update: aside-bar

--- public/assets/pages/js/nav.js
+++ public/assets/pages/js/nav.js
@@ -1,9 +1,96 @@
-document.querySelector('.header-menu').addEventListener('click', () => {
-    document.querySelector('.nav').classList.remove('nav-hidden');
-    document.querySelector('.nav-container').classList.remove('nav-hidden');
+document.querySelector(".header-menu").addEventListener("click", () => {
+    document.querySelector(".nav").classList.remove("nav-hidden");
+    document.querySelector(".nav-container").classList.remove("nav-hidden");
 });
 
-document.querySelector('.arrow-back').addEventListener('click', () => {
-    document.querySelector('.nav').classList.add('nav-hidden');
-    document.querySelector('.nav-container').classList.add('nav-hidden');
+document.querySelector(".arrow-back").addEventListener("click", () => {
+    document.querySelector(".nav").classList.add("nav-hidden");
+    document.querySelector(".nav-container").classList.add("nav-hidden");
+});
+
+const menuOpitions = document.querySelectorAll(".item-nav");
+
+menuOpitions.forEach((elem, index) => {
+    elem.addEventListener("click", (e) => {
+        e.preventDefault();
+        const container = document.querySelector(`.sub-category-${index}`);
+        const otherContainers = document.querySelectorAll(
+            `.sub-category:not(.sub-category-${index})`
+        );
+        const otherElements = Array.from(menuOpitions).filter(
+            (el) => el !== elem
+        );
+
+        switch (index) {
+            case 3:
+                otherContainers.forEach((c) =>
+                    c.classList.remove("sub-category-open-tree")
+                );
+                otherContainers.forEach((c) =>
+                    c.classList.remove("sub-category-open-four")
+                );
+                container.classList.toggle("sub-category-open-two");
+                otherElements.forEach((el) => el.classList.remove("active"));
+                elem.classList.toggle("active");
+                break;
+            case 4:
+                otherContainers.forEach((c) =>
+                    c.classList.remove("sub-category-open-two")
+                );
+                otherContainers.forEach((c) =>
+                    c.classList.remove("sub-category-open-three")
+                );
+                container.classList.toggle("sub-category-open-four");
+                otherElements.forEach((el) => el.classList.remove("active"));
+                elem.classList.toggle("active");
+                break;
+            case 5:
+                otherContainers.forEach((c) =>
+                    c.classList.remove("sub-category-open-two")
+                );
+                otherContainers.forEach((c) =>
+                    c.classList.remove("sub-category-open-three")
+                );
+                otherContainers.forEach((c) =>
+                    c.classList.remove("sub-category-open-four")
+                );
+                container.classList.toggle("sub-category-open-two");
+                otherElements.forEach((el) => el.classList.remove("active"));
+                elem.classList.toggle("active");
+                break;
+            case 6:
+                otherContainers.forEach((c) =>
+                    c.classList.remove("sub-category-open-two")
+                );
+                otherContainers.forEach((c) =>
+                    c.classList.remove("sub-category-open-three")
+                );
+                otherContainers.forEach((c) =>
+                    c.classList.remove("sub-category-open-four")
+                );
+                container.classList.toggle("sub-category-open-two");
+                otherElements.forEach((el) => el.classList.remove("active"));
+                elem.classList.toggle("active");
+                break;
+            default:
+                window.location.href = elem.children[1].href;
+                break;
+        }
+
+        /*     case 8:
+            otherContainers.forEach((c) =>
+                c.classList.remove("sub-category-open-two")
+            );
+            otherContainers.forEach((c) =>
+                c.classList.remove("sub-category-open-three")
+            );
+            otherContainers.forEach((c) =>
+                c.classList.remove("sub-category-open-four")
+            );
+            container.classList.toggle("sub-category-open-two");
+            otherElements.forEach((el) => el.classList.remove("active"));
+            elem.classList.toggle("active");
+            break;
+        */
+    });
 });
--- resources/views/layouts/sidebar-layout.blade.php
+++ resources/views/layouts/sidebar-layout.blade.php
@@ -1,16 +1,38 @@
-<div class="nav nav-hidden">
-    <div class="item-nav arrow-back"><span class="material-icons-outlined" id="icon-menu">arrow_back</span><a class="item-nav-text">Fechar</a></div>
-    <div class="item-nav"><span class="material-icons-outlined" id="icon-menu">home</span><a class="item-nav-text" href="{{ route('home.index') }}">Inicio</a></div>
-    <div class="item-nav"><span class="material-icons-outlined" id="icon-menu">grid_view</span><a class="item-nav-text" href="{{ route('dashboard.index') }}">Dashboard</a></div>
-    <div class="item-nav"><span class="material-icons-outlined" id="icon-menu">settings</span><a class="item-nav-text" href="{{ route('wallet.index') }}">Conta Digital</a></div>
-    <div class="item-nav"><span class="material-icons-outlined" id="icon-menu">star_outline</span><a class="item-nav-text" href="{{ route('store.edit') }}">Sua Loja</a></div>
-    <div class="item-nav"><span class="material-icons-outlined" id="icon-menu">sell</span><a class="item-nav-text" href="{{ route('produtos.index') }}">Produtos</a></div>
-    <div class="item-nav"><span class="material-icons-outlined" id="icon-menu">check_box</span><a class="item-nav-text" href="{{ route('pedidos.index') }}">Pedidos</a></div>
-    <div class="item-nav"><span class="material-icons-outlined" id="icon-menu">campaign</span><a class="item-nav-text" href="{{ route('pixel.index') }}">Marketing</a></div>
-    <div class="item-nav"><span class="material-icons-outlined" id="icon-menu">analytics</span><a class="item-nav-text" href="{{ route('growth_cov.index') }}">Growth</a></div>
-    <div class="item-nav"><span class="material-icons-outlined" id="icon-menu">settings_input_component</span><a class="item-nav-text" href="{{ route('integrations.index') }}">Integrações</a></div>
 
-    <div class="item-nav"><span class="material-icons-outlined" id="icon-menu">tune</span><a class="item-nav-text" href="{{ route('configuracao.edit') }}">Configurações</a></div>
+<div class="nav nav-hidden">
+    <div class="container-itens">
+        <div class="item-nav item-nav-logo"><img src="{{ URL::to('assets/pages/img/logo-recortada.png') }}" alt=""></div>
+        <div class="item-nav"><span class="material-icons-outlined" id="icon-menu">house</span><a class="item-nav-text" href="{{ route('home.index') }}">Inicio</a></div>
+        <div class="item-nav"><span class="material-icons-outlined" id="icon-menu">dashboard</span><a class="item-nav-text" href="{{ route('dashboard.index') }}">Dashboard</a></div>
+        <div class="item-nav"><span class="material-icons-outlined" id="icon-menu">manage_accounts</span><a class="item-nav-text" href="{{ route('wallet.index') }}">Conta Digital <img src="{{ URL::to('assets/pages/img/simple-arrow.svg') }}" alt=""></a></div>
+        <div class="sub-category sub-category-3">
+            <a href="{{ route('wallet.index') }}">Extrato</a>
+            <a href="{{ route('wallet.payment') }}">Pagamento</a>
+        </div>
+        <div class="item-nav"><span class="material-icons-outlined" id="icon-menu">store</span><a class="item-nav-text" href="{{ route('store.edit') }}">Sua Loja <img src="{{ URL::to('assets/pages/img/simple-arrow.svg') }}" alt=""></a></div>
+        <div class="sub-category sub-category-4">
+            <a href="{{ route('store.edit') }}">Dados</a>
+            <a href="#">Checkout</a>
+            <a href="#">Frete</a>
+            <a href="#">Domínio</a>
+        </div>
+        <div class="item-nav"><span class="material-icons-outlined" id="icon-menu">local_mall</span><a class="item-nav-text" href="{{ route('produtos.index') }}">Produtos <img src="{{ URL::to('assets/pages/img/simple-arrow.svg') }}" alt=""></a></div>
+        <div class="sub-category sub-category-5">
+            <a href="{{ route('produtos.create') }}">Cadastrar Produto</a>
+            <a href="#">Order Bumps</a>
+        </div>
+        <div class="item-nav"><span class="material-icons-outlined" id="icon-menu">receipt_long</span><a class="item-nav-text" href="{{ route('pedidos.index') }}">Pedidos <img src="{{ URL::to('assets/pages/img/simple-arrow.svg') }}" alt=""></a></div>
+        <div class="sub-category sub-category-6">
+            <a href="{{ route('pedidos.index') }}">Pedidos</a>
+            <a href="{{ route('pedidos.precart') }}">Carrinhos abandonados</a>
+        </div>
+        <div class="item-nav"><span class="material-icons-outlined" id="icon-menu">campaign</span><a class="item-nav-text" href="{{ route('pixel.index') }}">Marketing</a></div>
+        <div class="item-nav"><span class="material-icons-outlined" id="icon-menu">trending_up</span><a class="item-nav-text" href="{{ route('growth_cov.index') }}">Growth</a></div>
+        <div class="item-nav"><span class="material-icons-outlined" id="icon-menu">settings_input_component</span><a class="item-nav-text" href="{{ route('integrations.index') }}">Integrações</a></div>
+
+        <div class="item-nav"><span class="material-icons-outlined" id="icon-menu">tune</span><a class="item-nav-text" href="{{ route('configuracao.edit') }}">Configurações</a></div>
+        <div class="item-nav arrow-back"><span class="material-icons-outlined" id="icon-menu">arrow_back</span><a class="item-nav-text">Fechar</a></div>
+    </div>
 </div>
 
 <div class="nav-container nav-hidden"></div>
